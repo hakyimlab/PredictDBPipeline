2e168902ba7cde59df2cea06b28f39a5463a3b16
Scott Dickinson <spdickinson88@gmail.com>
2016-05-17 11:00:00 -0500

    update generate_sqlite_dbs.py to include model construction data

--- scripts/generate_sqlite_dbs.py
+++ scripts/generate_sqlite_dbs.py
@@ -1,14 +1,18 @@
 #!/usr/bin/env python
 
-SOURCE_DIR = 'input'
-TARGET_DIR = 'output'
-RESULTS_DIR = '/allResults'
-BETAS_DIR = '/allBetas'
+SOURCE_DIR = '../data/output/'
+TARGET_DIR = '../data/output/'
+RESULTS_DIR = 'allResults/'
+BETAS_DIR = 'allBetas/'
+LOGS_DIR = 'allLogs/'
+
 BETAS_INCLUDE_CLAUSE = ".allBetas."
 RESULTS_INCLUDE_CLAUSE = ".allResults."
+LOGS_INCLUDE_CLAUSE = '.allLogs.'
 
 BETA_HEADER_TOKENS = {"gene", "rsid", "ref", "alt", "beta", "alpha"}
 RESULTS_HEADER_TOKENS = {"gene", "alpha", "cvm", "lambda.iteration", "lambda.min", "n.snps", "R2", "pval", "genename"}
+LOGS_HEADER_TOKENS = {"chr", "n_samples", "n_genes", "seed_for_cv", "n_folds_cv", "snpset"}
 
 import gzip
 import os
@@ -25,7 +29,7 @@ def smart_open(source_file):
         sys.exit(1)
 
 def smart_list(source_dir, including):
-    source_files = [ x for x in os.listdir(source_dir) if including in x]
+    source_files = [x for x in os.listdir(source_dir) if including in x]
     if len(source_files) == 0:
         print "warning: no recognized source files (i.e., including %s) on %s"%(including, source_dir)
     return sorted(source_files)
@@ -142,7 +146,6 @@ def add_extra_data():
         def __init__(self, source_file, alpha, target_dir=TARGET_DIR):
             tissue_name = os.path.basename(source_file).split('.')[0]
             db_filename = os.path.join(target_dir, '%s_%s.db'%(tissue_name, alpha))
-            print(db_filename) ## delete after debug finished
             assert(os.path.exists(db_filename))
             self.connection = sqlite3.connect(db_filename)
 
@@ -189,13 +192,91 @@ def add_extra_data():
             meta_db.insert_row(row)
         meta_db.close()
 
+def add_log_data():
+    def data_rows_in(source_file):
+        "Iterate over data rows in the source file, labeling fields and converting formats as required."
+        def upconvert(x):
+            for f in (int, float):
+                try:
+                    return f(x)
+                except ValueError:
+                    pass
+            return x
+
+        header = None
+        for k, line in enumerate(smart_open(source_file)):
+            if k == 0:
+                if not LOGS_HEADER_TOKENS == set(line.strip().split()):
+                    raise RuntimeError("Invalid header. We no longer assume anything.")
+                header = line.strip().split()
+            else:
+                yield dict(zip(header, map(upconvert, line.strip().split())))
+
+    def source_files(source_dir=os.path.join(SOURCE_DIR,LOGS_DIR)):
+        "List all relevant source files"
+        for x in smart_list(source_dir, including=LOGS_INCLUDE_CLAUSE):
+            yield os.path.join(source_dir, x)
+
+    class DB:
+        "This encapsulates a single SQLite DB (for a given source file and alpha)."
+        def __init__(self, source_file, alpha, target_dir=TARGET_DIR):
+            tissue_name = os.path.basename(source_file).split('.')[0]
+            db_filename = os.path.join(target_dir, '%s_%s.db'%(tissue_name, alpha))
+            assert(os.path.exists(db_filename))
+            self.connection = sqlite3.connect(db_filename)
+
+            self("DROP INDEX IF EXISTS construction_chr")
+            self("DROP TABLE IF EXISTS construction")
+            self("CREATE TABLE construction (chr INTEGER, `n.samples` INTEGER, `n.genes` INTEGER, `cv.seed` INTEGER, `n.folds.cv` INTEGER, snpset TEXT)")
+            self("CREATE INDEX construction_chr ON construction (chr)")
+
+
+        def __call__(self, sql, args=None):
+            c = self.connection.cursor()
+            if args:
+                c.execute(sql, args)
+            else:
+                c.execute(sql)
+
+        def close(self):
+            self.connection.commit()
+
+        def insert_row(self, row):
+            self("INSERT INTO construction VALUES(?, ?, ?, ?, ?, ?)",
+                (row['chr'], row['n_samples'], row['n_genes'], row['seed_for_cv'], row['n_folds_cv'], row['snpset']))
+
+    class MetaDB:
+        "This handles all the DBs for each source file (tissue type)"
+        def __init__(self, source_file):
+            self.source_file = source_file
+            self.dbs = {} # alpha -> DB object
+
+        def insert_row(self, row):
+            # TODO: don't hardcode alpha. Necessary for now b/c logs file does not have alpha column.
+            alpha = 0.5
+            if alpha not in self.dbs:
+                self.dbs[alpha] = DB(self.source_file, alpha)
+            self.dbs[alpha].insert_row(row)
+
+        def close(self):
+            for db in self.dbs.values():
+                db.close()
+
+
+    for source_file in source_files():
+        print "Processing %s..."%source_file
+        meta_db = MetaDB(source_file=source_file)
+        for row in data_rows_in(source_file):
+            meta_db.insert_row(row)
+        meta_db.close()
+
 
 if __name__ == '__main__':
     import argparse
     parser = argparse.ArgumentParser(description='Create a model database from input files.')
 
     parser.add_argument("--input_folder",
-                        help="Folder containing -allBetas- and -allResults- input data",
+                        help="Folder containing -allBetas-, -allResults-, and -allLogs- input data",
                         default="input")
 
     parser.add_argument("--results_sub_folder",
@@ -205,6 +286,10 @@ if __name__ == '__main__':
     parser.add_argument("--betas_sub_folder",
                         help="Subfolder with -allBetas-",
                         default="allBetas")
+
+    parser.add_argument("--logs_sub_folder",
+                        help="Subfolder with -allLogs-",
+                        default="allLogs")
 
     parser.add_argument("--output_folder",
                         help="higher level output folder",
@@ -218,13 +303,20 @@ if __name__ == '__main__':
                         help="Pattern for results file name to adhere to",
                         default=".allResults.")
 
+    parser.add_argument("--logs_include_clause",
+                        help="Pattern for results file name to adhere to",
+                        default=".allLogs.")
+
     args = parser.parse_args()
     SOURCE_DIR = args.input_folder
     RESULTS_DIR = args.results_sub_folder
     BETAS_DIR = args.betas_sub_folder
+    LOGS_DIR = args.logs_sub_folder
     TARGET_DIR = args.output_folder
     BETAS_INCLUDE_CLAUSE = args.betas_include_clause
     RESULTS_INCLUDE_CLAUSE = args.results_include_clause
+    LOGS_INCLUDE_CLAUSE = args.logs_include_clause
 
     generate_weights_file()
     add_extra_data()
+    add_log_data()
