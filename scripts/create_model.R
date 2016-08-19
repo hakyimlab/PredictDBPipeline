f4b19155f739c604a8083806609a8bc13e5a0b04
Scott Dickinson <spdickinson88@gmail.com>
2016-08-19 15:58:26 -0500

    Fixed gene annotation for v6p

--- scripts/create_model.R
+++ scripts/create_model.R
@@ -10,7 +10,7 @@ data_dir <- "../data/intermediate/"
 
 expression_RDS <- data_dir %&% "expression_phenotypes/" %&% tis %&% "_Analysis.expr.RDS"
 geno_file <- data_dir %&% "genotypes/" %&% tis %&% "_chr" %&% chrom %&% "_Analysis.snps.biallelic.txt"
-gene_annot_RDS <- data_dir %&% "annotations/gene_annotation/gencode.v19.genes.no-exons.patched_contigs.parsed.RDS"
+gene_annot_RDS <- data_dir %&% "annotations/gene_annotation/gencode.v19.genes.v6p.patched_contigs.parsed.RDS"
 snp_annot_RDS <- data_dir %&% "annotations/snp_annotation/GTEx_OMNI_genot_1KG_imputed_var_info4_maf01_CR95_CHR_POSb37_ID_REF_ALT_release_v6.chr" %&% chrom %&% ".RDS"
 n_k_folds <- 10
 n_k_folds_rep <- 1
