8e249a00a29a379d94a4844f4e727cc716d5864a
Dickinson <spdickinson88@gmail.com>
2016-09-12 09:43:04 -0500

    linting parse_gtf.py

--- scripts/parse_gtf.py
+++ scripts/parse_gtf.py
@@ -1,6 +1,5 @@
 #! /usr/bin/env python
 
-import os
 import sys
 
 '''
@@ -10,30 +9,37 @@ position, and gene end position, and put in a tab delimited file.
 Runs from command line.  First argument is the gtf file, second is the
 output file.
 '''
-try: 
-    with open(sys.argv[1], 'r') as gtf:
-        with open(sys.argv[2], 'w') as out:
-            out_header = '\t'.join(['chr', 'gene_id', 'gene_name', 'start', 'end', 'gene_type']) + '\n'
-            out.write(out_header)
-            for line in gtf:
-                # Skip comments
-                if line[0] == '#':
-                    continue
-                gene_fields = line.split('\t')
-                # Exclude transcript and exon rows
-                if gene_fields[2] != 'gene':
-                    continue
-                gene_attributes = gene_fields[-1].split('; ')
-                attr_dict = dict(attribute.split() for attribute in gene_attributes if attribute)
-                # Some gtf files may have chromosome number with 'chr' prefix
-                # Just want number.
-                chr = gene_fields[0][3:] if "chr" in gene_fields[0] else gene_fields[0]
-                start = gene_fields[3]
-                end = gene_fields[4]
-                id = attr_dict['gene_id'].strip('"')
-                name = attr_dict['gene_name'].strip('"')
-                type = attr_dict['gene_type'].strip('"')
-                out_line = '\t'.join([chr, id, name, start, end, type]) + '\n'
-                out.write(out_line)
-except IOError as e:
-    print 'Operation failed: %s' % e.strerror
+
+HEADER_FIELDS = ['chr', 'gene_id', 'gene_name', 'start', 'end', 'gene_type']
+
+def parse_gtf(gtf_path, out_path):
+    with open(gtf_path, 'r') as gtf, open(out_path, 'w') as out:
+        out_header = '\t'.join(HEADER_FIELDS) + '\n'
+        out.write(out_header)
+        for line in gtf:
+            # Skip comments.
+            if line[0] == '#':
+                continue
+            gene_fields = line.split('\t')
+            # Exclude any rows not describing genes.
+            if gene_fields[2] != 'gene':
+                continue
+            gene_attributes = gene_fields[-1].split('; ')
+            attr_dict = dict(attribute.split() for attribute in gene_attributes if attribute)
+            # Some gtf files may have chromosome number with 'chr' prefix.
+            # We just need the number.
+            chr = gene_fields[0][3:] if "chr" in gene_fields[0] else gene_fields[0]
+            # Extract rest of desired fields.
+            start = gene_fields[3]
+            end = gene_fields[4]
+            id = attr_dict['gene_id'].strip('"')
+            name = attr_dict['gene_name'].strip('"')
+            type = attr_dict['gene_type'].strip('"')
+            # Concatenate together and write out to file.
+            out_line = '\t'.join([chr, id, name, start, end, type]) + '\n'
+            out.write(out_line)
+
+if __name__ == '__main__':
+    gtf_path = sys.argv[1]
+    out_path = sys.argv[2]
+    parse_gtf(gtf_path, out_path)
