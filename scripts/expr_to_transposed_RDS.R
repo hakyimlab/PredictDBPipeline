c3d92ff70e5b81b357457dee6d4640e0d3d90298
Dickinson <spdickinson88@gmail.com>
2016-09-12 16:47:27 -0500

    reorganized expr_to_transposed_RDS.R

--- scripts/expr_to_transposed_RDS.R
+++ scripts/expr_to_transposed_RDS.R
@@ -1,21 +1,33 @@
+# Reads an expression file in as a dataframe, transposes it, and
+# saves it as an RDS object.  If a covariate file is present (as 3rd
+# input argument), then it will be used to produce new expression data
+# by making a linear model expression ~ covariate, and then pulling the
+# residuals as the new expression data.
+#
+# The input expression file is expected to be tab-delimted, with people
+# as columns and genes as row.
+
 argv <- commandArgs(trailingOnly = TRUE)
 expressionfile <- argv[1]
 RDSout <- argv[2]
-# Input file has header as people ids, 1st col is gene_id.  Want transposed.
+# Presence of covariate file suggests to correct for PEER factors, etc.
+covariatefile <- ifelse(length(argv) == 3, argv[3], NULL)
+
 expression <- read.table(expressionfile, stringsAsFactors = FALSE,
     header = TRUE, row.names = 1)
+# Transpose expression.
 expression <- t(expression)
-# Presence of covariate file suggests to correct for PEER factors, etc.
-if (length(argv) == 3) {
-    covariatefile <- argv[3]
-    covariate <- read.table(covariatefile, stringsAsFactors = FALSE,
-    header = TRUE, row.names = 1)
-    covariate <- t(covariate)
 
-    for (i in 1:length(colnames(expression))) {
-        fit <- lm(expression[,i] ~ covariate)
-        expression[,i] <- fit$residuals
-    }
+if (!is.null(covariatefile)) {
+  # Correct expression data for covariates.
+  covariate <- read.table(covariatefile, stringsAsFactors = FALSE,
+    header = TRUE, row.names = 1)
+  covariate <- t(covariate)
+
+  for (i in 1:length(colnames(expression))) {
+    fit <- lm(expression[,i] ~ covariate)
+    expression[,i] <- fit$residuals
+  }
 }
 
 saveRDS(expression, RDSout)
