b6cc7a46d69857f2d467a5c158954d534eee386b
Scott Dickinson <spdickinson88@gmail.com>
2016-09-19 17:55:56 -0500

    more comments

--- example/preprocess_example.py
+++ example/preprocess_example.py
@@ -1,9 +1,39 @@
 #!/usr/bin/env python
 
-import os
 import subprocess
-import sys
 
+# Define parameters----------------------------------------------------/
+# Change these variables when adapting for different analyses.
+
+# List of identifiers for each database you'll make:
+STUDY_NAMES = ['gEUVADIS']
+# File names for gene and snp annotation:
+GENE_ANNOTATION_FN = 'gencode.v12.genes.gtf'
+SNP_ANNOTATION_FN = 'geuvadis.annot.txt'
+# List of genotype/expression file names:
+GENOTYPE_FNS = ['geuvadis.snps.txt']
+EXPRESSION_FNS = ['geuvadis.expr.txt']
+# File name of output for parse_gtf.py:
+GENE_ANNOT_INTER1 = 'gencode.v12.genes.parsed.txt'
+# File name of output for geno_annot_to_RDS.R:
+GENE_ANNOT_INTER2 = 'gencode.v12.genes.parsed.RDS'
+# File name prefix of outputs from split_snp_annot_by_chr.py:
+SNP_ANN_INTER_PREFIX1 = 'geuvadis.annot'
+# File name prefix for input files to snp_annot_to_RDS.R:
+SNP_ANN_INTER_PREFIX2 = 'geuvadis.annot.chr'
+# File name prefixes for output files from split_genotype_by_chr.py:
+GENOTYPE_INTER_PREFIX = ['geuvadis.snps']
+# File name prefixes for output files from expr_to_transposed_RDS.R:
+EXPR_INTER = ['geuvadis']
+
+# Model metadata/parameters:
+SNPSET = 'HapMap'
+ALPHA = '0.5'
+N_K_FOLDS = '10'
+RSID_LABEL = 'RSID_dbSNP137'
+WINDOW = '1e6'
+
+# Define directories---------------------------------------------------/
 INPUT_DIR = '../data/input/'
 INTER_DIR = '../data/intermediate/'
 OUTPUT_DIR = '../data/output/'
@@ -11,29 +41,6 @@ GENE_ANN_DIR = 'annotations/gene_annotation/'
 SNP_ANN_DIR = 'annotations/snp_annotation/'
 GENOTYPE_DIR = 'genotypes/'
 EXPRESSION_DIR = 'expression_phenotypes/'
-
-
-STUDY_NAMES = ['gEUVADIS']
-GENE_ANNOTATION_FN = 'gencode.v12.genes.gtf'
-SNP_ANNOTATION_FN = 'geuvadis.annot.txt'
-GENOTYPE_FNS = ['geuvadis.snps.txt']
-EXPRESSION_FNS = ['geuvadis.expr.txt']
-
-GENE_ANNOT_INTER1 = 'gencode.v12.genes.parsed.txt'
-GENE_ANNOT_INTER2 = 'gencode.v12.genes.parsed.RDS'
-
-SNP_ANN_INTER_PREFIX1 = 'geuvadis.annot'
-SNP_ANN_INTER_PREFIX2 = 'geuvadis.annot.chr'
-
-GENOTYPE_INTER_PREFIX = ['geuvadis.snps']
-
-EXPR_INTER = ['geuvadis']
-
-SNPSET = 'HapMap'
-ALPHA = '0.5'
-N_K_FOLDS = '10'
-RSID_LABEL = 'RSID_dbSNP137'
-WINDOW = '1e6'
 
 # Process gene annotation----------------------------------------------/
 print("Parsing gene annotation...")
@@ -77,8 +84,9 @@ for i in range(len(EXPRESSION_FNS)):
         ['Rscript', '../scripts/expr_to_transposed_RDS.R',
         INPUT_DIR + EXPRESSION_DIR + EXPRESSION_FNS[i],
         INTER_DIR + EXPRESSION_DIR + EXPR_INTER[i]])
-    
+
 # Create metadata files------------------------------------------------/
+print("Creating metadata files...")
 for i in range(len(STUDY_NAMES)):
     command = ' '.join(['../scripts/create_meta_data.py',
         '--geno', INPUT_DIR + GENOTYPE_DIR + GENOTYPE_FNS[i],
@@ -90,3 +98,5 @@ for i in range(len(STUDY_NAMES)):
         '--window', WINDOW,
         '--out_prefix', OUTPUT_DIR + 'allMetaData/' + STUDY_NAMES[i]])
     subprocess.call(command, shell=True)
+
+print("Done!")
